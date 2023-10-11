621459e9ee64386506a419d19311c364f4c86139
wray-lee <wray_lee@outlook.com>
2023-10-11 22:58:48 +0800

    update

--- Arch.sh
+++ Arch.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #
-read -p "Password" _password_
-echo $_password_ | sudo pacman -S --noconfirm zsh neofetch docker nginx
+read -p "Password:" _password_
+echo $_password_ | sudo pacman -S --noconfirm zsh neofetch docker nginx git
 sh -c "$(wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
@@ -12,3 +12,4 @@ wget -c https://raw.githubusercontent.com/wray-lee/one_click/main/.vimrc -O ~/.z
 sudo cp ~/.zshrc /root
 wget -c https://raw.githubusercontent.com/wray-lee/one_click/main/.p10k.zsh -O ~/.p10k.zsh
 sudo cp ~/.p10k.zsh /root
+chsh -s /usr/bin/zsh
--- Fedora.sh
+++ Fedora.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 #
 read -p "Password:" _password_
-echo $_password_ | sudo dnf install --assumeyes zsh neofetch docker nginx
+echo $_password_ | sudo dnf install --assumeyes zsh neofetch docker nginx git
 sh -c "$(wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 wget -c https://raw.githubusercontent.com/wray-lee/one_click/main/.vimrc -O ~/.vimrc
 sudo cp ~/.vimrc /root
-wget -c https://raw.githubusercontent.com/wray-lee/one_click/main/.vimrc -O ~/.zshrc
+wget -c https://raw.githubusercontent.com/wray-lee/one_click/main/.zshrc -O ~/.zshrc
 sudo cp ~/.zshrc /root
 wget -c https://raw.githubusercontent.com/wray-lee/one_click/main/.p10k.zsh -O ~/.p10k.zsh
 sudo cp ~/.p10k.zsh /root
+chsh -s /usr/bin/zsh
