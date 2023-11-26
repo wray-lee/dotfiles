75aa769ad184bb3feac07809298536938dbe0e8b
wray-lee <wray_lee@outlook.com>
2023-11-26 23:45:31 +0800

    Add Ubuntu Support

---
+++ Ubuntu.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#
+read -p "Password:" _password_
+echo $_password_ | sudo sed -i 's@//.*archive.ubuntu.com@//mirrors.ustc.edu.cn@g' /etc/apt/sources.list
+echo $_password_ | sudo apt install -y zsh neofetch docker nginx git ranger bat tldr
+sh -c "$(wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+wget -c https://raw.githubusercontent.com/wray-lee/one_click/main/.vimrc -O ~/.vimrc
+sudo cp ~/.vimrc /root
+sed -i '1i|neofetch' .
+sudo sed -e '1i neofetch' -e 's|ZSH_THEME="robbyrussell"|ZSH_THEME="powerlevel10k/powerlevel10k"|g' -e 's|plugins=(git)|plugins=(git zsh-autosuggestions zsh-syntax-highlighting)|g' -i ~/.zshrc
+sudo cp ~/.zshrc /root
+wget -c https://raw.githubusercontent.com/wray-lee/one_click/main/.p10k.zsh -O ~/.p10k.zsh
+sudo cp ~/.p10k.zsh /root
+sudo cp -r ./.oh-my-zsh /root/.oh-my-zsh
+chsh -s /usr/bin/zsh
+chsh -s /usr/bin/zsh root
