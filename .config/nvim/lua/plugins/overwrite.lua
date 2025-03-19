255a28233c1783e493a1feb8e5ce8cbc261ab185
wray-lee <wray_lee@outlook.com>
2025-03-19 11:31:08 +0800

    Add nvim config

---
+++ .config/nvim/lua/plugins/overwrite.lua
@@ -0,0 +1,12 @@
+return {
+  {
+    "AstroNvim/astrocore",
+    ---@type AstroCoreOpts
+    opts = { -- extend the plugin options
+      diagnostics = {
+        -- disable diagnostics virtual text
+        virtual_text = false,
+      },
+    },
+  },
+}
