0ad06649e2b96f533096e92ea5d6d4abe704ac4e
wray-lee <wray_lee@outlook.com>
2025-09-27 22:47:44 +0800

    Update

--- fixChinese.sh
+++ fixChinese.sh
@@ -4,3 +4,4 @@ sed -ie 's/\/usr\/bin\/code %F/\/usr\/bin\/code --ozone-platform-hint=auto --ena
 sed -ie 's/\/opt\/bilibili-appimage\/bilibili.AppImage --no-sandbox %U/\/opt\/bilibili-appimage\/bilibili.AppImage --no-sandbox --ozone-platform-hint=auto --enable-wayland-ime --wayland-text-input-version=3 %U/g' /usr/share/applications/bilibili.desktop
 sed -ie 's/\/opt\/lx-music-desktop\/lx-music-desktop %U/\/opt\/lx-music-desktop\/lx-music-desktop --ozone-platform-hint=auto --enable-wayland-ime --wayland-text-input-version=3 %U/g' /usr/share/applications/lx-music-desktop.desktop
 sed -ie 's/\/usr\/bin\/linuxqq --no-sandbox %U/\/usr\/bin\/linuxqq --ozone-platform-hint=auto --enable-wayland-ime --wayland-text-input-version=3 %U/g' /usr/share/applications/linuxqq.desktop
+sed -ie 's/typora %U/typora --ozone-platform-hint=auto --enable-wayland-ime --wayland-text-input-version=3 %U/g' /usr/share/applications/typora.desktop
