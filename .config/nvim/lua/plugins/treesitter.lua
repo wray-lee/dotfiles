43fdae9c3dd539524d51551dd7d0089ddc233abd
wray-lee <wray_lee@outlook.com>
2026-04-09 23:50:37 +0800

    Update

---
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -0,0 +1,16 @@
+-- if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
+
+-- Customize Treesitter
+
+---@type LazySpec
+return {
+  "nvim-treesitter/nvim-treesitter",
+  enabled = false,
+  opts = {
+    ensure_installed = {
+      "lua",
+      "vim",
+      -- add more arguments for adding more treesitter parsers
+    },
+  },
+}
